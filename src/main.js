70b1517c520c8173aee02685dea1bedf954b8917
Roy <1455730594@qq.com>
2021-04-14 00:44:12 +0800

    加入vue-router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
 import snackbar from './plugins/snackbar'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
 Vue.use(snackbar)
 
 new Vue({
-	vuetify,
+    vuetify,
+    router: new VueRouter({
+        mode: 'hash',
+        routes: [{
+            path: '/',
+            name: 'App',
+            component: () => import('./App')
+        }]
+    }),
 	render: h => h(App)
 }).$mount('#app')
